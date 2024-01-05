ada06dc4d0c0ba4711d622f7b36ab5bb191745a4
Gaurab Upreti <58771387+notsogaurab@users.noreply.github.com>
2024-01-05 10:45:15 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,64 +1,72 @@
-// Create a web server that can respond to requests for /comments.json 
-// with a JSON representation of the comments list given above. 
-// Make sure to add the necessary HTTP header to make the client understand 
-// that the response contains JSON.
-
-// You will need to use the JSON.stringify function to convert your object 
-// data to a string before sending it to the client.
-
-// You can learn more about Node.js modules here.
-
-var http = require('http');
+// Create web servervar express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
 var fs = require('fs');
-var url = require('url');
-var comments = require('./comments.js');
+var cors = require('cors');
+var PORT = 3001;
 
-var server = http.createServer(function (request, response) {
-  var urlObj = url.parse(request.url, true);
-  var pathname = urlObj.pathname;
-  var query = urlObj.query;
-  if (pathname === '/') {
-    fs.readFile('./index.html', function (err, data) {
-      if (err) {
-        response.writeHead(404, 'Not Found');
-        response.end();
-      } else {
-        response.setHeader('Content-Type', 'text/html;charset=utf-8');
-        response.end(data);
-      }
-    });
-  } else if (pathname === '/index.css') {
-    fs.readFile('./index.css', function (err, data) {
-      if (err) {
-        response.writeHead(404, 'Not Found');
-        response.end();
-      } else {
-        response.setHeader('Content-Type', 'text/css;charset=utf-8');
-        response.end(data);
-      }
-    });
-  } else if (pathname === '/index.js') {
-    fs.readFile('./index.js', function (err, data) {
-      if (err) {
-        response.writeHead(404, 'Not Found');
-        response.end();
-      } else {
-        response.setHeader('Content-Type', 'application/javascript;charset=utf-8');
-        response.end(data);
-      }
-    });
-  } else if (pathname === '/comments.json') {
-    response.setHeader('Content-Type', 'application/json;charset=utf-8');
-    response.end(JSON.stringify(comments));
-  } else if (pathname === '/add') {
-    comments.push(query);
-    response.end();
-  } else {
-    response.writeHead(404, 'Not Found');
-    response.end();
-  }
+// Set up CORS
+app.use(cors());
+
+// Set up body parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Set up static files
+app.use(express.static('public'));
+
+// Set up routes
+app.get('/comments', function(req, res) {
+  fs.readFile('comments.json', function(err, data) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(JSON.parse(data));
+    }
+  });
 });
 
-server.listen(3000, function () {
-  console.log('server listen port 3000');
+app.post('/comments', function(req, res) {
+  var newComment = req.body;
+  fs.readFile('comments.json', function(err, data) {
+    if (err) {
+      console.log(err);
+    } else {
+      var comments = JSON.parse(data);
+      comments.push(newComment);
+      fs.writeFile('comments.json', JSON.stringify(comments), function(err) {
+        if (err) {
+          console.log(err);
+        } else {
+          res.json(comments);
+        }
+      });
+    }
+  });
+});
+
+app.delete('/comments/:id', function(req, res) {
+  var id = req.params.id;
+  fs.readFile('comments.json', function(err, data) {
+    if (err) {
+      console.log(err);
+    } else {
+      var comments = JSON.parse(data);
+      comments = comments.filter(function(comment) {
+        return comment.id !== id;
+      });
+      fs.writeFile('comments.json', JSON.stringify(comments), function(err) {
+        if (err) {
+          console.log(err);
+        } else {
+          res.json(comments);
+        }
+      });
+    }
+  });
+});
+
+// Start server
+app.listen(PORT, function() {
+  console.log('Server listening on port ' + PORT);
 });
